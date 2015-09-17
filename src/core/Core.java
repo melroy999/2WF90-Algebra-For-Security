1d5f90f3f4f3563622fb8bd4e6993727873b0770
Melroy van Nijnatten <m.v.nijnatten@student.tue.nl>
2015-09-17 19:52:40 +0200

    Woops, completely broke it...

--- src/core/Core.java
+++ src/core/Core.java
@@ -10,11 +10,23 @@ public class Core {
         Polynomial p = new Polynomial("2X^2 + 4X + 1");
         System.out.println(p.toString());
 
-        p = new Polynomial("2X^2 + 4X + 1 + 5X - X^2");
-        System.out.println(p.toString());
+        Polynomial q = new Polynomial("2X^2 + 4X + 1 + 5X - X^2");
+        System.out.println(q.toString());
 
-        p = new Polynomial("2X^2 + 4X + 1 + 5X - 3X^2 + X^3");
-        System.out.println(p.toString());
+        Polynomial r = new Polynomial("2X^2 + 4X + 1 + 5X - 3X^2 + X^3");
+        System.out.println(r.toString());
+
+        System.out.println(p.add(q).toString());
+        System.out.println(p.subtract(q).toString());
+        System.out.println(p.multiply(q).toString());
+        System.out.println("Degree: " + p.multiply(q).getDegree());
+        System.out.println("Base for p exp:2: " + p.getCoefficient(2));
+        System.out.println("Base for p exp:3: " + p.getCoefficient(3));
+
+        Polynomial t = new Polynomial("X^4 + 2X^3 + 3X^2 + 2X + 1");
+        Polynomial s = new Polynomial("X^2 - 1");
+
+        System.out.println("Division of t by s: " + t.division(s).quotient.toString() + " " + t.division(s).remainder.toString());
     }
 
     public static void main(String[] args){
--- src/polynomial/Polynomial.java
+++ src/polynomial/Polynomial.java
@@ -14,133 +14,185 @@ public class Polynomial {
     //use TreeMap to store the polynomial
     private TreeMap<Integer, Integer> polynomial = new TreeMap<Integer, Integer>();
 
-    public Polynomial(String poly) {
-        toPolynomial(poly);
+    private Polynomial() {
     }
 
-    private Polynomial(TreeMap<Integer, Integer> poly){
-        this.polynomial = poly;
+    public Polynomial(String polynomial) {
+        toPolynomial(polynomial);
     }
 
-    public String toString(){
+    private Polynomial(TreeMap<Integer, Integer> polynomial) {
+        this.polynomial = polynomial;
+    }
+
+    public String toString() {
         String standardForm = "";
         Iterator<Integer> iterator = polynomial.descendingKeySet().iterator();
-        while(iterator.hasNext()){
-            int exponent = iterator.next();
-            int base = polynomial.get(exponent);
-            standardForm += (base == 1 && exponent != 0 ? "" : base);
-            standardForm += (exponent == 0 ? "" : "X");
-            standardForm += (exponent > 1 ? "^" + exponent : "");
-            standardForm += (iterator.hasNext()? " + " : "");
+        while (iterator.hasNext()) {
+            int degree = iterator.next();
+            int coefficient = polynomial.get(degree);
+            standardForm += (coefficient == 1 && degree != 0 ? "" : coefficient);
+            standardForm += (degree == 0 ? "" : "X");
+            standardForm += (degree > 1 ? "^" + degree : "");
+            standardForm += (iterator.hasNext() ? " + " : "");
         }
-        standardForm = standardForm.replace("+ -","-");
+        standardForm = standardForm.replace("+ -", "-");
         return standardForm;
     }
 
-    public int getDeg(){
+    public int getDegree() {
         return polynomial.lastKey();
     }
 
-    public int getBase(int exp){
-        int base = 0;
-        if(polynomial.containsKey(exp)) {
-            base = polynomial.get(exp);
+    public int getCoefficient(int degree) {
+        int coefficient = 0;
+        if (polynomial.containsKey(degree)) {
+            coefficient = polynomial.get(degree);
         }
-        return base;
+        return coefficient;
     }
 
-    private void toPolynomial(String poly) {
+    protected void add(int coefficient, int degree) {
+        if (polynomial.containsKey(degree)) {
+            coefficient += polynomial.get(degree);
+        }
+
+        if(coefficient == 0){
+            polynomial.remove(degree);
+        } else {
+            polynomial.put(degree, coefficient);
+        }
+    }
+
+    private void toPolynomial(String polynomial) {
         /**
          * Could be improved / made more robust.
          */
-        poly = poly.replace(" ","");
-        poly = poly.replaceAll("[^0-9\\^\\+\\-]", "x");
-        poly = poly.replace("-", "+-");
-        poly = poly.replace("-x", "-1x");
-        poly = poly.replace("+x","+1x");
-        String[] parts = poly.split("\\+");
-        for (String part : parts) {
-            int deg;
-            int base;
-            if(part.contains("^")){
-                part = part.replace("x","");
-                String[] components = part.split("\\^");
-                base = Integer.parseInt(components[0]);
-                deg = Integer.parseInt(components[1]);
+        polynomial = polynomial.replace(" ", "");
+        polynomial = polynomial.replaceAll("[^0-9\\^\\+\\-]", "x");
+        polynomial = polynomial.replace("-", "+-");
+        polynomial = polynomial.replace("-x", "-1x");
+        polynomial = polynomial.replace("+x", "+1x");
+        if(polynomial.charAt(0) == 'x'){
+            polynomial = "1" + polynomial;
+        }
+        String[] terms = polynomial.split("\\+");
+        for (String term : terms) {
+            int exp;
+            int coefficient;
+            if (term.contains("^")) {
+                term = term.replace("x", "");
+                String[] components = term.split("\\^");
+                coefficient = Integer.parseInt(components[0]);
+                exp = Integer.parseInt(components[1]);
             } else {
-                if(part.contains("x")){
-                    part = part.replace("x","");
-                    deg = 1;
+                if (term.contains("x")) {
+                    term = term.replace("x", "");
+                    exp = 1;
                 } else {
-                    deg = 0;
+                    exp = 0;
                 }
-                base = Integer.parseInt(part);
+                coefficient = Integer.parseInt(term);
             }
-            if(polynomial.containsKey(deg)){
-                base += polynomial.get(deg);
-            }
-            polynomial.put(deg, base);
+            add(coefficient, exp);
         }
     }
 
-    public TreeMap<Integer, Integer> getTreeMap(){
+    public TreeMap<Integer, Integer> getTreeMap() {
         return new TreeMap<Integer, Integer>(polynomial);
     }
 
-    public int size(){
+    public int size() {
         return polynomial.size();
     }
 
-    public Polynomial add(Polynomial poly){
-        TreeMap<Integer, Integer> firstMap;
-        TreeMap<Integer, Integer> secondMap;
-        if(poly.size() < polynomial.size()){
-            firstMap = this.getTreeMap();
-            secondMap = poly.getTreeMap();
+    public Polynomial add(Polynomial polynomial2) {
+        TreeMap<Integer, Integer> result;
+        TreeMap<Integer, Integer> addition;
+        if (polynomial2.size() < polynomial2.size()) {
+            result = this.getTreeMap();
+            addition = polynomial2.getTreeMap();
         } else {
-            firstMap = new TreeMap<Integer, Integer>(poly.getTreeMap());
-            secondMap = this.getTreeMap();
+            result = new TreeMap<Integer, Integer>(polynomial2.getTreeMap());
+            addition = this.getTreeMap();
         }
 
-        Iterator<Integer> iterator = secondMap.keySet().iterator();
-        while(iterator.hasNext()){
+        Iterator<Integer> iterator = addition.keySet().iterator();
+        while (iterator.hasNext()) {
             int exponent = iterator.next();
-            int base = polynomial.get(exponent);
-            if(firstMap.containsKey(exponent)){
-                firstMap.put(exponent, base + firstMap.get(exponent));
-            } else {
-                firstMap.put(exponent, base);
-            }
-        }
-
-        return new Polynomial(firstMap);
-    }
-
-    public Polynomial subtract(Polynomial poly){
-        TreeMap<Integer, Integer> result = this.getTreeMap();
-        Iterator<Integer> iterator = poly.getTreeMap().keySet().iterator();
-        while(iterator.hasNext()){
-            int exponent = iterator.next();
-            int base = result.get(exponent);
-            if(result.containsKey(exponent)){
-                result.put(exponent, base - result.get(exponent));
-            } else {
-                result.put(exponent, -base);
-            }
+            int coefficient = polynomial.get(exponent);
+            add(coefficient, exponent);
         }
 
         return new Polynomial(result);
     }
 
-    public Polynomial multiply(Polynomial poly){
-        return null;
+    public Polynomial subtract(Polynomial polynomial2) {
+        TreeMap<Integer, Integer> result = this.getTreeMap();
+        Iterator<Integer> iterator = polynomial2.getTreeMap().keySet().iterator();
+        while (iterator.hasNext()) {
+            int exponent = iterator.next();
+            if(!result.containsKey(exponent)){
+                //as items can be removed...
+                continue;
+            }
+            int base = result.get(exponent);
+            add(-base, exponent);
+            /*if (result.containsKey(exponent)) {
+                result.put(exponent, base - result.get(exponent));
+            } else {
+                result.put(exponent, -base);
+            }*/
+        }
+
+        return new Polynomial(result);
     }
 
-    public Polynomial divide(Polynomial poly){
-        return null;
+    /**
+     * !Make this use a more efficient algorithm
+     */
+    public Polynomial multiply(Polynomial polynomial2) {
+        Polynomial result = new Polynomial();
+        TreeMap<Integer, Integer> multiply = polynomial2.getTreeMap();
+        for (int exp1 : polynomial.keySet()) {
+            for (int exp2 : polynomial2.getTreeMap().keySet()) {
+                int base1 = polynomial.get(exp1);
+                int base2 = multiply.get(exp2);
+                result.add(base1 * base2, exp1 + exp2);
+            }
+        }
+        return result;
     }
 
-    public Polynomial remainder(Polynomial poly){
+    public Pair division(Polynomial polynomial2) {
+        Polynomial q = new Polynomial();
+        Polynomial r = new Polynomial(new TreeMap<Integer, Integer>(polynomial));
+        while(r.getDegree() >= polynomial2.getDegree()){
+            q.add(r.getLeadingCoefficient() / polynomial2.getLeadingCoefficient(), r.getDegree() - polynomial2.getDegree());
+
+            Polynomial temp = new Polynomial();
+            temp.add(r.getLeadingCoefficient() / polynomial2.getLeadingCoefficient(), r.getDegree() - polynomial2.getDegree());
+            temp.multiply(polynomial2);
+            r.subtract(temp);
+        }
+        return new Pair(q, r);
+    }
+
+    public int getLeadingCoefficient(){
+        return getCoefficient(getDegree());
+    }
+
+    public class Pair {
+        public Polynomial quotient;
+        public Polynomial remainder;
+
+        public Pair(Polynomial quotient, Polynomial remainder) {
+            this.quotient = quotient;
+            this.remainder = remainder;
+        }
+    }
+
+    public Pair gcd(Polynomial polynomial2) {
         return null;
     }
 }
