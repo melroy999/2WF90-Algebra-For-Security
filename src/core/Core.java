24ae9905c914a876a30218d49c88c302338ab840
melroy999 <m.v.nijnatten@student.tue.nl>
2015-09-24 11:17:51 +0200

    Cleaned up.... again!

--- src/core/Core.java
+++ src/core/Core.java
@@ -1,19 +1,10 @@
 package core;
 
-import polynomial.Polynomial;
-import polynomial.support.implementation.DoubleNumeric;
-import polynomial.support.implementation.FloatNumeric;
-import polynomial.support.implementation.IntegerNumeric;
-import polynomial.support.implementation.ModNumeric;
-
 /**
  * Created by Melroy van Nijnatten - 0849740.
  */
 public class Core {
     public Core() {
-        Polynomial<Integer> p = new Polynomial<Integer>(new ModNumeric(2), "3X^2");
-        Polynomial<Integer> q = new Polynomial<Integer>(new ModNumeric(2), "3X^2");
-        System.out.println(p.add(q));
     }
 
     public static void main(String[] args){
