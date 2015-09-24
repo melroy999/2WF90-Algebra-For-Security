d0d69f1851f78678750f5eca95c03fbf33304860
Melroy van Nijnatten <m.v.nijnatten@student.tue.nl>
2015-09-24 22:11:05 +0200

    Some more bullshit... parsing stuff...

--- src/core/Core.java
+++ src/core/Core.java
@@ -9,6 +9,7 @@ public class Core {
     public Core() {
         Parser.parse("X^2 + 2X");
         Parser.parse("9(X^2 + 6X^3 + 12X^5) - X^2");
+        Parser.parse("-11(-X^13(-X^3 - 4)) - (X^2 + 4X^2)");
     }
 
     public static void main(String[] args){
--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -14,10 +14,27 @@ public class Parser {
     public static void parse(String toParse){
         toParse = toParse.trim();
         toParse = toParse.replaceAll("[^0-9\\+\\-*()^]", "");
-        //toParse = toParse.replace("-","+-");
         toParse = toParse.replace("+^", "+1^");
         toParse = toParse.replace("-^", "-1^");
         toParse = toParse.replace("(^", "(1^");
+
+        String[] split = toParse.split("\\(");
+        for(int i = 1; i < split.length; i++){
+            split[i] = "(" + split[i];
+            char c = split[i-1].charAt(split[i-1].length()-1);
+            if(Character.isDigit(c)) {
+                split[i - 1] = split[i - 1] + "*";
+            } else if(c == '-'){
+                split[i - 1] = split[i - 1] + "1*";
+            } else if(c == '+'){
+
+            }
+        }
+
+        toParse = "";
+        for (String aSplit : split) {
+            toParse = toParse + aSplit;
+        }
 
         //use regular expressions to give it the correct format
         //eg aa+b*c+ -> aaaaabccc, aabbbbbc etc
