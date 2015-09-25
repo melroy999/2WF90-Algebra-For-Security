467e97e3a89809cd3a8aa834caabe74da53219f1
melroy999 <m.v.nijnatten@student.tue.nl>
2015-09-25 19:39:30 +0200

    More parsing.

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -13,21 +13,31 @@ import java.util.regex.Pattern;
 public class Parser {
     public static void parse(String toParse){
         toParse = toParse.trim();
-        toParse = toParse.replaceAll("[^0-9\\+\\-*()^]", "");
+        toParse = toParse.toUpperCase().replaceAll("[A-Y]", "X");
+        toParse = toParse.replaceAll("X([^\\^]|\\Z)", "X^1");
+        //toParse = toParse.replaceAll("(((?<=\\+|\\-|\\*)([0-9]+)(?!X))|((?<!\\^)([0-9]+)(?=\\+|\\-|\\*|\\))))", "\g<1>X^0");
+        //TODO look into this...
+
+        toParse = toParse.replaceAll("[^0-9\\+\\-*()^X]", "");
         toParse = toParse.replace("+^", "+1^");
         toParse = toParse.replace("-^", "-1^");
+        toParse = toParse.replace("*^", "*1^");
         toParse = toParse.replace("(^", "(1^");
+        toParse = toParse.startsWith("^") ? "1" + toParse : toParse;
 
         String[] split = toParse.split("\\(");
         for(int i = 1; i < split.length; i++){
             split[i] = "(" + split[i];
+            if(split[i-1].equals("")){
+                continue;
+            }
             char c = split[i-1].charAt(split[i-1].length()-1);
             if(Character.isDigit(c)) {
                 split[i - 1] = split[i - 1] + "*";
             } else if(c == '-'){
                 split[i - 1] = split[i - 1] + "1*";
-            } else if(c == '+'){
-
+            } else if(c == ')'){
+                split[i - 1] = split[i - 1] + "*";
             }
         }
 
@@ -52,6 +62,9 @@ public class Parser {
         for (String aSplit : split) {
             toParse = toParse + aSplit;
         }
+
+        split = toParse.split("\\*");
+
 
         //use regular expressions to give it the correct format
         //eg aa+b*c+ -> aaaaabccc, aabbbbbc etc
