f464b7a089346e1fa0cae37e0ca34ee24fdbde27
Melroy van Nijnatten <m.v.nijnatten@student.tue.nl>
2015-10-11 00:52:58 +0200

    Added comments to Core.

--- src/core/Core.java
+++ src/core/Core.java
@@ -14,27 +14,30 @@ public class Core {
     public static PrintHandler printHandler;
 
     public Core() {
+        //do some display settings.
         UIManager.put("TabbedPane.contentBorderInsets", new Insets(1, 0, 1, 0));
         UIManager.put("TabbedPane.selected", Color.white);
         UIManager.put("TabbedPane.focus", Color.white);
 
+        //create the jframe.
         JFrame frame = new JFrame("Assignment 2WF90 - October 2015 - Stefan Habets & Melroy van Nijnatten");
         guiCore = new GUICore();
 
+        //register the printHandler.
         printHandler = new PrintHandler(guiCore);
         Thread.setDefaultUncaughtExceptionHandler(Core.printHandler);
 
+        //set the content of the frame.
         frame.setContentPane(guiCore.mainPane);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+        //display trhe frame
         frame.pack();
         frame.setVisible(true);
     }
 
     public static void main(String[] args){
-
-
         new Core();
     }
 }
