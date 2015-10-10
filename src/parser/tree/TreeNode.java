af125ff6cb0038331a6bb49f0531594a83bdb546
Melroy van Nijnatten <m.v.nijnatten@student.tue.nl>
2015-10-11 00:57:34 +0200

    Added comments to TreeNode.

--- src/parser/tree/TreeNode.java
+++ src/parser/tree/TreeNode.java
@@ -12,47 +12,100 @@ public class TreeNode {
         this.value = value;
     }
 
+    /**
+     * Get the value of this node.
+     *
+     * @return the string value of this node.
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Get the left child node.
+     *
+     * @return the left child node.
+     */
     public TreeNode getLeftNode(){
         return child[0];
     }
 
+    /**
+     * Set the left child node.
+     *
+     * @param node: The node that should be the left child.
+     * @return this node.
+     */
     public TreeNode setLeftNode(TreeNode node) {
         this.child[0] = node;
         node.setParent(this);
         return this;
     }
 
+    /**
+     * Get the right child node.
+     *
+     * @return the right child node.
+     */
     public TreeNode getRightNode(){
         return child[1];
     }
 
+    /**
+     * Set the right child node.
+     *
+     * @param node: The node that should be the right child.
+     * @return this node.
+     */
     public TreeNode setRightNode(TreeNode node) {
         this.child[1] = node;
         node.setParent(this);
         return this;
     }
 
+    /**
+     * Check if the node is a leaf.
+     *
+     * @return check if both children are absent.
+     */
     public boolean hasChildren() {
         return child[0] != null || child[1] != null;
     }
 
+    /**
+     * Check if this node has a parent.
+     *
+     * @return if parent is null.
+     */
     public boolean hasParent() {
         return parent != null;
     }
 
+    /**
+     * Returns the parent of this node.
+     *
+     * @return the parent.
+     */
     public TreeNode getParent(){
         return parent;
     }
 
+    /**
+     * Sets the parent.
+     *
+     * @param node: Node that should be the parent.
+     * @return this node.
+     */
     public TreeNode setParent(TreeNode node) {
         this.parent = node;
         return this;
     }
 
+    /**
+     * Returns both children.
+     *
+     * @return Array containing both children.
+     */
     public TreeNode[] getChildren(){
         return child;
     }
