5d72d25e71bbf56a2e37673e6b89f2ce3ee26d2c
steef2404 <steef2404@hotmail.com>
2015-10-21 11:47:14 +0200

    2 is a prime again. Yeah

--- src/polynomial/Arithmetic.java
+++ src/polynomial/Arithmetic.java
@@ -256,11 +256,11 @@ public class Arithmetic {
      * @return If n is prime or not.
      */
     public static boolean isPrime(int n) {
-        if (n == 0 || n == 1 || n % 2 == 0) {
-            return false;
-        }
         if (n == 2) {
             return true;
+        }
+        if (n == 0 || n == 1 || n % 2 == 0) {
+            return false;
         }
         for (int i = 3; i <= Math.sqrt(n); i += 2) {
             if (n % i == 0) {
